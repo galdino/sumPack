9f1964991710f21654fb1ef3acdb9dd59e995cbb
FERNANDO GALDINO DA SILVA FILHO <T01POR0117@unimedfortaleza.com.br>
2018-09-19 10:06:18 -0300

    first commit

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' Sums two numbers
+#'
+#' This function sums tow input numbers.
+#' @param x,y The two numbers.
+#' @return The sum of the two numbers.
+#' @export
+#' @examples
+#' sumNum(1,2)
+
+sumNum <- function(x,y) {
+x+y
+}
